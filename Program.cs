8123fef136399246f78b0046e8871b9cfa07e5ca
Michael <fireflyjackal@gmail.com>
2019-02-11 21:50:29 -0500

    Add program data.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GenerationLib;
 
 namespace CaveGeneratorDemo
 {
@@ -10,6 +7,41 @@ namespace CaveGeneratorDemo
     {
         static void Main(string[] args)
         {
+            int seed = System.Environment.TickCount;
+            int timeStart = System.Environment.TickCount;
+            float ratio = 0.3f;
+
+            Console.Out.WriteLine("Seed: " + seed);
+            Console.Out.WriteLine();
+
+            Console.Out.WriteLine("Without Connecting Rooms");
+            CaveGenerator caveFactory = new CaveGenerator(79, 50, seed, ratio);
+            caveFactory.GenerateMap(false);
+            PrintMap(caveFactory);
+
+            Console.Out.WriteLine("With Connecting Rooms");
+            caveFactory = new CaveGenerator(79, 50, seed, ratio);
+            caveFactory.GenerateMap(true);
+            PrintMap(caveFactory);
+
+            Console.Out.WriteLine();
+            Console.Out.WriteLine("Time To Generate: " + ((System.Environment.TickCount - timeStart) / 1000).ToString() + "s");
+
+            Console.In.ReadLine();
         }
+        public static void PrintMap(CaveGenerator map)
+        {
+            for (int row = 1; row <= map.Map.Rows; row++) {
+                for (int column = 1; column <= map.Map.Columns; column++) {
+                    if (map.Map.GetTile(column, row) == CaveGenerator.TileWall || map.Map.GetTile(column, row) == CaveGenerator.TilePermanantWall) {
+                        Console.Write("#");
+                    } else {
+                        Console.Write(".");
+                    }
+                } //column
+
+                Console.Write(System.Environment.NewLine);
+            } //row
+        } // PrintGrid 
     }
 }
